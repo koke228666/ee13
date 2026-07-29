80e6873b389778d7d8cac18db67c745aeb22d269
koke228666 <146654074+koke228666@users.noreply.github.com>
2026-07-30 02:28:29 +0300

    stub

--- core/al_routes/al_profileEdit/a_save_general.php
+++ core/al_routes/al_profileEdit/a_save_general.php
@@ -2,8 +2,8 @@
 return $ee13vars->ajax(0, [[
         'only_name'     => false,
         'name_response' => '',
-        'status'        => (int) $status,
-        'partner'       => (int) $partnerId,
+        'status'        => 0,
+        'partner'       => 0,
         'family'        => [],
         'relation_text' => '',
 ], '']);
